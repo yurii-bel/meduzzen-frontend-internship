dc3d7b8e072d1530611a7f5bbbf3c5ae17850f7f
Yurii <yuriibel888@gmail.com>
2023-04-26 20:44:30 +0300

    feat: add pages

--- src/Pages/CompaniesList.tsx
+++ src/Pages/CompaniesList.tsx
@@ -1,10 +1,31 @@
-import React from "react";
+import { companies } from "../Data/companiesData";
+
+interface Company {
+  id: number;
+  name: string;
+  industry: string;
+  city: string;
+  state: string;
+}
 
 const CompaniesList: React.FC = () => {
   return (
-    <div>
-      <h1>Companies List</h1>
-    </div>
+    <section>
+      <h1 className="text-3xl font-bold mb-4 ml-10">Companies List</h1>
+      <div className="p-4 flex justify-center">
+        <ul>
+          {companies.map((company) => (
+            <li key={company.id} className="mb-4">
+              <h2 className="text-xl font-bold">{company.name}</h2>
+              <p className="text-gray-600 mb-2">Industry: {company.industry}</p>
+              <p className="text-gray-600">
+                Location: {company.city}, {company.state}
+              </p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
   );
 };
 
