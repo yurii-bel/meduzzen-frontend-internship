2b8d9a7aba0259fdd384307f27f8da9a0e0df2fc
Yurii <yuriibel888@gmail.com>
2023-04-26 16:06:11 +0300

    feat: add Footer.tsx component

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <footer>
+      <p>&copy; {new Date().getFullYear()} Yurii Beliavtsev</p>
+    </footer>
+  );
+};
+
+export default Footer;
