337922fe7e408a30cc1a24f845dbcfd3d60a11ff
Yurii <yuriibel888@gmail.com>
2023-04-26 18:00:37 +0300

    style: formatting code

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Layout from "./components/layout/Layout";
-import HomePage from "./pages/HomePage";
-import About from "./pages/About";
-import Authentication from "./pages/Authentication";
-import Registration from "./pages/Registration";
-import UsersList from "./pages/UsersList";
-import UserProfile from "./pages/UserProfiel";
-import CompaniesList from "./pages/CompaniesList";
-import CompanyProfile from "./pages/CompanyProfile";
+import Layout from "./Components/Layout/Layout";
+import HomePage from "./Pages/HomePage";
+import About from "./Pages/About";
+import Authentication from "./Pages/Authentication";
+import Registration from "./Pages/Registration";
+import UsersList from "./Pages/UsersList";
+import UserProfile from "./Pages/UserProfiel";
+import CompaniesList from "./Pages/CompaniesList";
+import CompanyProfile from "./Pages/CompanyProfile";
 
 const App: React.FC = () => {
   return (
---
+++ src/Components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer: React.FC = () => {
+  return (
+    <footer>
+      <p>&copy; {new Date().getFullYear()} Yurii Beliavtsev</p>
+    </footer>
+  );
+};
+
+export default Footer;
