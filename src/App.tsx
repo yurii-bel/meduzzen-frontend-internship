30496b201cb325ca82c6f4f0bb861ca2f1b581b4
Yurii <yuriibel888@gmail.com>
2023-04-26 17:01:54 +0300

    feat: add Routes and Layout to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
-import HomePage from "./Pages/HomePage";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+import HomePage from "./pages/HomePage";
+import About from "./pages/About";
+import Authentication from "./pages/Authentication";
+import Registration from "./pages/Registration";
+import UsersList from "./pages/UsersList";
+import UserProfile from "./pages/UserProfiel";
+import CompaniesList from "./pages/CompaniesList";
+import CompanyProfile from "./pages/CompanyProfile";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <HomePage title="Tasks Front End 4.0" />
-    </div>
+    <Router>
+      <div>
+        <Layout>
+          <Routes>
+            <Route
+              path="/"
+              element={<HomePage title="Tasks Front End 4.0" />}
+            />
+            <Route path="/about" element={<About />} />
+            <Route path="/auth" element={<Authentication />} />
+            <Route path="/registration" element={<Registration />} />
+            <Route path="/users-list" element={<UsersList />} />
+            <Route path="/user-profile" element={<UserProfile />} />
+            <Route path="/companies-list" element={<CompaniesList />} />
+            <Route path="/company-profile" element={<CompanyProfile />} />
+          </Routes>
+        </Layout>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
