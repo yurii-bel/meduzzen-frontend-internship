c6853c9d440265c5cd485a334814df9ab917280e
Yurii <yuriibel888@gmail.com>
2023-04-26 20:43:21 +0300

    feat: add Header and Footer

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,7 +1,13 @@
+import React from "react";
+
 const Footer: React.FC = () => {
   return (
-    <footer>
-      <p>&copy; {new Date().getFullYear()} Yurii Beliavtsev</p>
+    <footer className="fixed bottom-0 left-0 right-0 bg-gray-800 text-white p-4 text-center">
+      <div className="container mx-auto px-4">
+        <p className="text-center">
+          &copy; {new Date().getFullYear()} Yurii Beliavtsev
+        </p>
+      </div>
     </footer>
   );
 };
--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,23 +1,35 @@
-import React from "react";
 import { Link } from "react-router-dom"; // assuming you're using React Router
 
 const Header: React.FC = () => {
   return (
-    <header>
-      <nav>
-        <ul>
+    <header className="bg-gray-800 mb-4">
+      <nav className="container flex justify-between gap-10 mx-auto py-4">
+        <ul className="flex justify-start items-center text-white">
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/" className="font-bold text-xl">
+              Home
+            </Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link to="/about" className="ml-4 hover:text-gray-300">
+              About
+            </Link>
           </li>
-          <li>
-            <Link to="/auth">Sign in</Link>
-          </li>
-          <li>
-            <Link to="/registration">Sign up</Link>
-          </li>
+        </ul>
+        <ul className="flex justify-start items-center gap-4 text-white">
+          <Link
+            to="/auth"
+            className="px-4 py-2 bg-white text-gray-800 rounded-md hover:bg-gray-300"
+          >
+            Sign in
+          </Link>
+
+          <Link
+            to="/registration"
+            className="px-4 py-2 bg-white text-gray-800 rounded-md hover:bg-gray-300"
+          >
+            Sign up
+          </Link>
         </ul>
       </nav>
     </header>
