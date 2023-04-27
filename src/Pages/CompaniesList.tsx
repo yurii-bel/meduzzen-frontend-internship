1731e119b48706b9991c4a77ed0c2195b13873a9
Yurii <yuriibel888@gmail.com>
2023-04-27 11:09:33 +0300

    refactor: refactor Lists

--- src/Pages/CompaniesList.tsx
+++ src/Pages/CompaniesList.tsx
@@ -1,4 +1,5 @@
 import { companies } from "../Data/companiesData";
+import Companies from "../Components/Companies";
 
 interface Company {
   id: number;
@@ -14,15 +15,7 @@ const CompaniesList: React.FC = () => {
       <h1 className="text-3xl font-bold mb-4 ml-10">Companies List</h1>
       <div className="p-4 flex justify-center">
         <ul>
-          {companies.map((company) => (
-            <li key={company.id} className="mb-4">
-              <h2 className="text-xl font-bold">{company.name}</h2>
-              <p className="text-gray-600 mb-2">Industry: {company.industry}</p>
-              <p className="text-gray-600">
-                Location: {company.city}, {company.state}
-              </p>
-            </li>
-          ))}
+          <Companies companies={companies} />
         </ul>
       </div>
     </section>
--- src/Pages/UsersList.tsx
+++ src/Pages/UsersList.tsx
@@ -1,42 +1,10 @@
 import { users } from "../Data/usersData";
+import Users from "../Components/Users";
 
 const UsersList = () => {
   return (
     <section className="flex justify-evenly items-center">
-      {users.map((user) => (
-        <div key={user.result.user_id}>
-          <div className="border rounded p-4">
-            <div className="flex items-center mb-4">
-              <img
-                className="h-12 w-12 rounded-full mr-4"
-                src={user.result.user_avatar}
-                alt="User Avatar"
-              />
-              <div>
-                <h2 className="text-lg font-bold">{`${user.result.user_firstname} ${user.result.user_lastname}`}</h2>
-                <p className="text-gray-600">{user.result.user_city}</p>
-              </div>
-            </div>
-            <div className="mb-4">
-              <p className="text-gray-600">Email: {user.result.user_email}</p>
-              <p className="text-gray-600">Phone: {user.result.user_phone}</p>
-              <p className="text-gray-600">Status: {user.result.user_status}</p>
-            </div>
-            <div className="mb-4">
-              <h3 className="text-lg font-bold">Links:</h3>
-              <ul className="list-disc list-inside">
-                {user.result.user_links.map((link, index) => (
-                  <li key={index}>
-                    <a href={link} className="text-blue-500 hover:underline">
-                      {link}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </div>
-        </div>
-      ))}
+      <Users users={users} />
     </section>
   );
 };
