993d474c060af587d37d322717daa6e7a76f963d
Yurii <yuriibel888@gmail.com>
2023-05-03 11:01:03 +0300

    feat: update header

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,47 +1,82 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { RootState } from "../Store";
+import LogoutButton from "./LogoutButton";
 
 type NavItem = {
   title: string;
   path: string;
 };
 
-const navItems: NavItem[] = [
+const navItemsUserLoggedIn: NavItem[] = [
   { title: "Home", path: "/" },
   { title: "About", path: "/about" },
-  { title: "Auth", path: "/auth" },
-  { title: "Registration", path: "/registration" },
   { title: "UsersList", path: "/users-list" },
-  { title: "UserProfile", path: "/user-profile" },
-  { title: "CompaniesList", path: "/companies-list" },
-  { title: "CompanyProfile", path: "/company-profile" },
+  // { title: "Auth", path: "/auth" },
+  // { title: "Registration", path: "/registration" },
+  // { title: "UserProfile", path: "/user-profile" },
+  // { title: "CompaniesList", path: "/companies-list" },
+  // { title: "CompanyProfile", path: "/company-profile" },
+];
+
+const navItemsUserNotLoggedIn: NavItem[] = [
+  { title: "Home", path: "/" },
+  { title: "About", path: "/about" },
 ];
 
 const Header: React.FC = () => {
+  const userEmail = useSelector((state: RootState) => state.user.user_email);
+  // console.log("user: " + userEmail);
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
+  useEffect(() => {
+    userEmail ? setUserLoggedIn(true) : setUserLoggedIn(false);
+  }, [userEmail]);
+
+  // console.log(userLoggedIn);
+
   return (
     <header className="bg-gray-800 mb-4">
       <nav className="container flex justify-between gap-10 mx-auto py-4">
         <ul className="flex justify-start items-center text-white gap-4">
-          {navItems.map((item) => (
-            <li key={item.path}>
-              <Link to={item.path} className="hover:text-gray-300">
-                {item.title}
-              </Link>
-            </li>
-          ))}
+          {userLoggedIn
+            ? navItemsUserLoggedIn.map((item) => (
+                <li key={item.path}>
+                  <Link to={item.path} className="hover:text-gray-300">
+                    {item.title}
+                  </Link>
+                </li>
+              ))
+            : navItemsUserNotLoggedIn.map((item) => (
+                <li key={item.path}>
+                  <Link to={item.path} className="hover:text-gray-300">
+                    {item.title}
+                  </Link>
+                </li>
+              ))}
         </ul>
         <ul className="flex justify-start items-center gap-4 text-white">
-          <Link
-            to="/auth"
-            className="px-3 py-1 bg-white text-gray-800 rounded-md hover:bg-gray-300"
-          >
-            Sign in
-          </Link>
-          <Link
-            to="/registration"
-            className="px-3 py-1 bg-white text-gray-800 rounded-md hover:bg-gray-300"
-          >
-            Sign up
-          </Link>
+          {!userLoggedIn ? (
+            <>
+              <Link
+                to="/auth"
+                className="px-3 py-1 bg-white text-gray-800 rounded-md hover:bg-gray-300"
+              >
+                Sign in
+              </Link>
+              <Link
+                to="/registration"
+                className="px-3 py-1 bg-white text-gray-800 rounded-md hover:bg-gray-300"
+              >
+                Sign up
+              </Link>
+            </>
+          ) : (
+            <>
+              <p>{userEmail}</p>
+              <LogoutButton />
+            </>
+          )}
         </ul>
       </nav>
     </header>
