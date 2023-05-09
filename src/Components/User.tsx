b7ff8c26b28b3022f6cfec11e9b6baacea39bf8c
Yurii <yuriibel888@gmail.com>
2023-05-09 11:00:05 +0300

    feat: update user component

--- src/Components/User.tsx
+++ src/Components/User.tsx
@@ -1,26 +1,18 @@
 import React from "react";
 import { UserProps } from "../Types/types";
-import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 const User: React.FC<UserProps> = ({ user }) => {
-  const navigate = useNavigate();
-
-  const handleUserClick = (userId: number) => {
-    navigate(`/user-profile/${userId}`);
-  };
-
   return (
     <Link to={`/user-profile/${user.user_id}`}>
-      <div
-        className="border rounded p-4 hover:bg-purple-50 hover:border-purple-600 hover:cursor-pointer duration-100"
-        key={user.user_id}
-        onClick={() => handleUserClick(user.user_id)}
-      >
+      <div className="border border-gray-300 rounded p-4 hover:bg-purple-50 hover:border-purple-600 hover:cursor-pointer duration-100">
         <div className="flex items-center mb-4">
           <img
-            className="h-12 w-12 rounded-full mr-4"
-            src={user.user_avatar}
+            className="h-12 w-12  mr-4 object-cover"
+            src={
+              user.user_avatar ||
+              "https://assets.stickpng.com/images/585e4bf3cb11b227491c339a.png"
+            }
             alt="User Avatar"
           />
           <div>
@@ -29,17 +21,23 @@ const User: React.FC<UserProps> = ({ user }) => {
           </div>
         </div>
         <div className="mb-4">
-          <p className="text-gray-600">Email: {user.user_email}</p>
-          <p className="text-gray-600">Phone: {user.user_phone}</p>
-          <p className="text-gray-600">Status: {user.user_status}</p>
+          <p className="text-gray-600">
+            <span className="font-bold">Email:</span> {user.user_email}
+          </p>
+          <p className="text-gray-600">
+            <span className="font-bold">Phone:</span> {user.user_phone}
+          </p>
+          <p className="text-gray-600">
+            <span className="font-bold">Status:</span> {user.user_status}
+          </p>
         </div>
         <div className="mb-4">
           <h3 className="text-lg font-bold">Links:</h3>
           {/* <ul className="list-disc list-inside">
-            {user.user_links.map((link, index) => (
-              <UserLinksItem link={link} key={index} />
-            ))}
-          </ul> */}
+      {user.user_links.map((link, index) => (
+        <UserLinksItem link={link} key={index} />
+      ))}
+    </ul> */}
         </div>
       </div>
     </Link>
