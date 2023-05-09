6d03809ae94d96953a543fa4b0c875aa9361688c
Yurii <yuriibel888@gmail.com>
2023-05-09 20:21:06 +0300

    feat: update universal  button

--- src/Components/Core/Button.tsx
+++ src/Components/Core/Button.tsx
@@ -11,7 +11,9 @@ const Button = ({ label, onClick, disabled }: ButtonProps) => {
     <button
       onClick={onClick}
       disabled={disabled}
-      className="relative w-full min-w-[100px] flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-purple-800 hover:bg-purple-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500"
+      className={`${
+        disabled ? "cursor-not-allowed bg-purple-400" : ""
+      } relative w-full min-w-[100px] flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-purple-800 hover:bg-purple-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500`}
     >
       {label}
     </button>
