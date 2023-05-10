6663556196d13f7797f38b78bbdb058da2db29db
Yurii <yuriibel888@gmail.com>
2023-05-10 11:28:22 +0300

    feat: update

--- src/Pages/UsersList.tsx
+++ src/Pages/UsersList.tsx
@@ -5,6 +5,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { setUsers } from "../Store/usersListReducer";
 import { RootState } from "../Store";
 import Button from "../Components/Core/Button";
+import Pagination from "../Components/Pagination";
 
 const UsersList: React.FC = () => {
   const dispath = useDispatch();
@@ -44,43 +45,6 @@ const UsersList: React.FC = () => {
     setCurrentPage(totalPages);
   };
 
-  const renderPageNumbers = () => {
-    const pageNumbers = [];
-    const maxPages = 11; // Maximum number of page numbers to display
-
-    let startPage = 1;
-    let endPage = totalPages;
-
-    if (totalPages > maxPages) {
-      if (currentPage <= maxPages - 5) {
-        endPage = maxPages - 1;
-      } else if (currentPage >= totalPages - 5) {
-        startPage = totalPages - maxPages + 5;
-      } else {
-        startPage = currentPage - 5;
-        endPage = currentPage + 5;
-      }
-    }
-
-    for (let i = startPage; i <= endPage; i++) {
-      pageNumbers.push(
-        <button
-          key={i}
-          onClick={() => setCurrentPage(i)}
-          className={`px-2 py-1 rounded ${
-            i === currentPage
-              ? "bg-purple-800 text-white"
-              : "bg-white text-purple-800"
-          }`}
-        >
-          {i}
-        </button>
-      );
-    }
-
-    return pageNumbers;
-  };
-
   return (
     <section className="flex flex-col mt-4 mb-24">
       <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 gap-4 px-12 mb-4">
@@ -101,7 +65,13 @@ const UsersList: React.FC = () => {
             disabled={currentPage === 1}
           />
         </div>
-        <div>{renderPageNumbers()}</div>
+        <div>
+          <Pagination
+            currentPage={currentPage}
+            totalPages={totalPages}
+            setCurrentPage={setCurrentPage}
+          />
+        </div>
         <div className="flex gap-1">
           <Button
             label="Next"
