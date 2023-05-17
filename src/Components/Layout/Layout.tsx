e57a0deb4d52b5541ab6ef5930534a67a0df5ed4
Yurii <yuriibel888@gmail.com>
2023-05-17 22:37:49 +0300

    feat: update layout

--- src/Components/Layout/Layout.tsx
+++ src/Components/Layout/Layout.tsx
@@ -11,7 +11,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
     <div>
       <Header />
       <main>{children}</main>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 };
