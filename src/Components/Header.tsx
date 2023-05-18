b6b90a18cbbf1c7e9cd08d651a70e0bdf66c9c62
Yurii <yuriibel888@gmail.com>
2023-05-18 15:44:20 +0300

    style: remove comments

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -21,7 +21,6 @@ const Header: React.FC = () => {
     { title: "Users", path: "/users-list" },
     { title: "Profile", path: `/user-profile/${userId}` },
     { title: "Companies", path: "/companies-list" },
-    // { title: "My Companies", path: `/my-companies-list/${userId}` },
   ];
 
   const navItemsUserNotLoggedIn: NavItem[] = [
