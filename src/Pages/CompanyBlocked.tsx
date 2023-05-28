8a7c8f64950d1955f3a16279104613ddbe8ee40a
Yurii <yuriibel888@gmail.com>
2023-05-28 19:21:04 +0300

    Remove all files

--- src/Pages/CompanyBlocked.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const CompanyBlocked: React.FC = () => {
-  return <div>CompanyBlocked</div>;
-};
-
-export default CompanyBlocked;
--- src/Store/store.ts
+++
@@ -1,7 +0,0 @@
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import rootReducer from "./";
-
-const store = createStore(rootReducer, applyMiddleware(thunk));
-
-export default store;
