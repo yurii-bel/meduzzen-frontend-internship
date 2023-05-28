4ee7ecda3bc3ca130af183343600ad10a508b427
Yurii <yuriibel888@gmail.com>
2023-05-28 18:17:04 +0300

    feat: update

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -31,15 +31,15 @@ const HomePage: React.FC<Props> = ({ title }) => {
           <h1 className="text-4xl font-bold mb-5">{title}</h1>
           <h2 className="text-2xl font-bold mb-5">Greetings!</h2>
           <p className="text-gray-700 leading-loose">
-            Liorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-            eu nisi vel sapien euismod molestie ac vel dolor. Sed eu elit et
-            eros efficitur auctor. Morbi lobortis velit ut ex pellentesque, vel
-            pulvinar magna commodo. Praesent commodo ornare justo, id dapibus
-            augue hendrerit sed. Sed sodales faucibus libero, ac eleifend nisl
-            consectetur eu. Sed hendrerit orci vel enim ultrices, eu aliquet
-            eros hendrerit. Nam elementum sapien at justo bibendum, ac ultricies
-            odio sollicitudin. Morbi non ullamcorper augue. Sed in dui at quam
-            volutpat aliquam.
+            Lssiorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Vestibulum eu nisi vel sapien euismod molestie ac vel dolor. Sed eu
+            elit et eros efficitur auctor. Morbi lobortis velit ut ex
+            pellentesque, vel pulvinar magna commodo. Praesent commodo ornare
+            justo, id dapibus augue hendrerit sed. Sed sodales faucibus libero,
+            ac eleifend nisl consectetur eu. Sed hendrerit orci vel enim
+            ultrices, eu aliquet eros hendrerit. Nam elementum sapien at justo
+            bibendum, ac ultricies odio sollicitudin. Morbi non ullamcorper
+            augue. Sed in dui at quam volutpat aliquam.
           </p>
           <Counter />
           <div className="flex flex-col mt-2 mb-16 rounded-md bg-gray-800 text-white">
